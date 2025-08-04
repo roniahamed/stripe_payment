29466f3e2552cae7d58e4a433a8c1b70b2f552d5
roniahamed <mdroniahamed56@gmail.com>
2025-08-05 00:20:17 +0600

    Product

--- product/models.py
+++ product/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User 
 
 # Create your models here.
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    user = models.ForeignKey(User, on_delete= models.CASCADE, related_name='products')
+    price = models.PositiveIntegerField()
+    descriptions = models.CharField(max_length=100)
