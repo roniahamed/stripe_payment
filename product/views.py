e725d0ed112822d556602425f76476eedd5c08bc
roniahamed <mdroniahamed56@gmail.com>
2025-08-05 05:39:21 +0600

    update

--- product/views.py
+++ product/views.py
@@ -3,10 +3,15 @@ from .serializers import UserSerializer
 from rest_framework.permissions import AllowAny, IsAuthenticated, IsAuthenticatedOrReadOnly
 from rest_framework import generics
 
+# stripe
+import stripe
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Product, Order
+
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
 
-    
 
