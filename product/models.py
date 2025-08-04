d3159ff6de7181412539f60d13c6346ac9b06142
roniahamed <mdroniahamed56@gmail.com>
2025-08-05 03:06:40 +0600

    Order model

--- product/models.py
+++ product/models.py
@@ -9,3 +9,15 @@ class Product(models.Model):
     user = models.ForeignKey(User, on_delete= models.CASCADE, related_name='products')
     price = models.PositiveIntegerField()
     descriptions = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='order')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='product')
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_paid = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'Order number: {self.id}, by {self.user.username} '
