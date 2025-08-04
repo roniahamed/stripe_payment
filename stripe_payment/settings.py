0b05425b717652029e59c76de82bb2b6162ac88c
roniahamed <mdroniahamed56@gmail.com>
2025-08-05 01:21:56 +0600

    using Jwt

--- stripe_payment/settings.py
+++ stripe_payment/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     # Rest Framework
 
     'rest_framework',
+    'rest_framework_simplejwt',
 
     #additional app
     'product',
@@ -136,7 +137,10 @@ REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ]
 }
+
+
 
