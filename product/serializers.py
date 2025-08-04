01a0094327d74e2245874594946b19392285cece
roniahamed <mdroniahamed56@gmail.com>
2025-08-05 03:25:35 +0600

    User Serializer

--- product/serializers.py
+++ product/serializers.py
@@ -1,5 +1,12 @@
 from rest_framework import serializers
 from .models import Product, Order
+from django.contrib.auth.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'password']
+        extra_kwargs = {'password':{'write_only':True}}
 
 class OrderSerializer(serializers.ModelSerializer):
     class Meta:
