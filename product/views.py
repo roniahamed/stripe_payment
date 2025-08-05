185ea570adb51f7a39cf275ad4535fb1db1b57d7
roniahamed <mdroniahamed56@gmail.com>
2025-08-05 06:26:46 +0600

    checkout session

--- product/views.py
+++ product/views.py
@@ -15,5 +15,37 @@ class RegisterView(generics.CreateAPIView):
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
 
+
 stripe.api_key = settings.api_key = settings.STRIPE_SECRET_KEY
+
+class CreateCheckoutSessionView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, *args, **kwargs):
+        product_id = self.kwargs.get('pk')
+        try:
+            product = Product.objects.get(pk=product_id)
+            checkout_session = stripe.checkout.Session.create(
+                payment_method_types = ['card'],
+                line_items = [{
+                    'price_data':{
+                        'currency':'usd',
+                        'product_data': {'name':product.name,},
+                        'unit_amount':int(product.price * 100),
+                    },
+                    'quantity':1,
+            }
+                ]
+            )
+            mode = 'payment',
+            success_url = 'http://localhost:8000/success/',
+            cancel_url = 'http://localhost:8000/cancel/',
+            metadata = {
+                'user_id': request.user.id,
+                'product_id':product.id
+            }
+
+            return Response({'checkout_url':checkout_session})
+        except Product.DoesNotExist:
+            return Response({'error':'Product not found'})
 
