069f07ade38e5c83cb317139debcb6fd8ce2c27a
roniahamed <mdroniahamed56@gmail.com>
2025-08-06 05:57:26 +0600

    setup smtp gmail

--- stripe_payment/settings.py
+++ stripe_payment/settings.py
@@ -149,3 +149,14 @@ REST_FRAMEWORK = {
 STRIPE_PUBLISHABLE_KEY = config('STRIPE_PUBLISHABLE_KEY')
 STRIPE_SECRET_KEY = config('STRIPE_SECRET_KEY')
 STRIPE_WEBHOOK_SECRET = config('STRIPE_WEBHOOK_SECRET')
+
+
+# Email 
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True 
+EMAIL_HOST_USER = 'roniahamed.jvai@gmail.com'
+EMAIL_HOST_PASSWORD = config('SMTP_PASSWORD')
+DEFAULT_FROM_EMAIL = 'roniahamed.jvai@gmail.com'
