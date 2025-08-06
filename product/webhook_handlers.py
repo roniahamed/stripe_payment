827233661a4c5bf2bbf2e2f1dc4e8d530de706b4
roniahamed <mdroniahamed56@gmail.com>
2025-08-07 04:25:27 +0600

    handle payment failure notification

--- product/webhook_handlers.py
+++ product/webhook_handlers.py
@@ -46,9 +46,46 @@ def handle_payment_success(session_data):
         logger.error(f'An unexpected error occurred: {e}', exc_info=True)
 
 
+# handle payment failure
 @transaction.atomic
-def handle_payment_failure(session):
-    pass
+def handle_payment_failure(session_data):
+    """
+    Handler for the 'checkout.session.async_payment_failed' event.
+    Logs the reason for the failed payment and notifies the user via email.
+    """
+    session_id = session_data.get('id')
+    logger.error(f'Asynchronous payment failed for checkout session {session_id}')
+
+    metadata = session_data.get('metadata', {})
+    user_id = metadata.get('user_id')
+    product_id = metadata.get('product_id')
+
+    try:
+        user = User.objects.get(pk = user_id)
+        products = Product.objects.get(pk=product_id)
+
+        # Creating an "order-like" object for the email template
+        # because at this stage no order has been created in the database.
+
+        class TempOrder:
+            id = 'Incomplete'
+            product = products
+
+        send_order_email(user, TempOrder(), 'Your payment has failed', './payment_failure_email.html')
+
+    except User.DoesNotExist:
+        logger.error(f'User ID {user_id} not found while sending payment failure notification.')
+    except Product.DoesNotExist:
+        logger.error(f'Product ID {product_id} not found while sending payment failure notification.')
+    except Exception as e:
+        logger.error(f'An error occurred while sending the payment failure notification')
+        
+
+
+
+
+
+
 @transaction.atomic
 def handle_session_expired(session):
     pass
