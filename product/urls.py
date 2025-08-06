8dd1c2070f64db77bd3ae1fccf1341c99545d70d
roniahamed <mdroniahamed56@gmail.com>
2025-08-07 02:14:40 +0600

    adding success and cancel urls

--- product/urls.py
+++ product/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RegisterView, ProductListView, ProductDetailView, CreateCheckoutSessionView, StripeWebhookView
+from .views import RegisterView, ProductListView, ProductDetailView, CreateCheckoutSessionView, StripeWebhookView, PaymentCancelView, PaymentSuccessView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
@@ -7,4 +7,6 @@ urlpatterns = [
     path('product/<int:pk>/', ProductDetailView.as_view(), name='product-detail view'),
     path('product/<int:pk>/create-checkout-session/', CreateCheckoutSessionView.as_view(),name='create-checkout-session'),
     path('stripe-webhook/', StripeWebhookView.as_view(), name='stripe-webhook' ),
+    path('success/', PaymentSuccessView.as_view(), name='success'),
+    path('cancel/', PaymentCancelView.as_view(), name='cancel'),
 ]
