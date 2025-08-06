a0d661a0c2dc1d80b483ec47cd2484cf359065a3
roniahamed <mdroniahamed56@gmail.com>
2025-08-07 04:35:40 +0600

    Refunds payment

--- product/webhook_handlers.py
+++ product/webhook_handlers.py
@@ -79,16 +79,42 @@ def handle_payment_failure(session_data):
         logger.error(f'Product ID {product_id} not found while sending payment failure notification.')
     except Exception as e:
         logger.error(f'An error occurred while sending the payment failure notification')
-        
-
-
-
-
-
 
 @transaction.atomic
 def handle_session_expired(session):
     pass
+
+
+# Handle refunds 
+
+
 @transaction.atomic
-def handle_refund(charge):
-    pass 
+def handle_refund(refund_data):
+    payment_intent_id = refund_data.get('payment_intent')
+    logger.info(f'Refund Webhook received: {payment_intent_id}')
+
+    if not payment_intent_id:
+        logger.error(f'Error: Could not find payment_intent_id in refund data')
+        return 
+
+    try:
+        order = Order.objects.get(payment_intent_id=payment_intent_id)
+
+        # If the order is already marked as refunded, nothing to do.
+        # Prevents duplicate webhook handling.
+        if order.is_refunded:
+            logger.warning(f'Order : {order.id} is already marked as refunded. Webhook ignored.')
+            return 
+        order.is_refunded = True
+        order.save()
+        logger.info(f'Order #{order.id} successfully marked as refunded.')
+
+        send_order_email(order.user, order, 'Your order has been refunded', './refund_processed_email.html' )
+    except Order.DoesNotExist:
+        logger.error(f'Critical: No order found for payment_intent_id {payment_intent_id}.')    
+    except Exception as e:
+        logger.error(f'An unexpected error occurred while handling the refund: {e}', exc_info=True)
+        
+        
+
+    
