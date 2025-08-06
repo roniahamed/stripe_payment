d23c2e8679e52bee77602a9958ee76f63b135411
roniahamed <mdroniahamed56@gmail.com>
2025-08-06 06:12:34 +0600

    import importent pakage

--- product/webhook_handlers.py
+++ product/webhook_handlers.py
@@ -1,15 +1,21 @@
 from django.db import transaction
-from .models import Order
+from .models import Order, Product
+import logging
+from django.contrib.auth.models import User
+from .email_utils import send_order_email
+
+
+logger = logging.getLogger(__name__)
 
 @transaction.atomic
 def handle_payment_success(session):
-    pass
-
+    
+@transaction.atomic
 def handle_payment_failure(session):
     pass
-
+@transaction.atomic
 def handle_session_expired(session):
     pass
-
+@transaction.atomic
 def handle_refund(charge):
     pass 
