7e5ddbebf1c24a4da819879e484919bd2cdde125
roniahamed <mdroniahamed56@gmail.com>
2025-08-06 06:44:53 +0600

    handle success payment by stripe

--- product/webhook_handlers.py
+++ product/webhook_handlers.py
@@ -7,9 +7,45 @@ from .email_utils import send_order_email
 
 logger = logging.getLogger(__name__)
 
+# Payment Successful 
 @transaction.atomic
-def handle_payment_success(session):
+def handle_payment_success(session_data):
+    metadata = session_data.get('metadata', {})
+    user_id = metadata.get('user_id')
+    product_id = metadata.get('product_id')
+    payment_intent_id = session_data.get('payment_intent')
+    if not all([user_id, product_id, payment_intent_id]):
+        logger.error(f"Error: Required metadata not found from session {session_data.get('id')}.")
+
+    try:
+        user = User.objects.get(pk=user_id)
+        product = Product.objects.get(id=product_id)
+
+        order, created = Order.objects.updated_or_create(
+            payment_intent_id = payment_intent_id,
+            defaults = {
+                'user': user,
+                'product':product,
+                'is_paid':True,
+                'is_refunded':False
+            }
+        )
+        
+        if created:
+            logger.info(f"New order #{order.id} created.")
+        else:
+            logger.info(f"New order #{order.id} updated.")
+        
+        send_order_email(user, order, 'Your order has been successful!', './order_success_email.html')
+    except User.DoesNotExist:
+        logger.error(f" Critical Error: User ID {user_id} not found in the database.")
     
+    except Product.DoesNotExist:
+        logger.error(f'Critical Error: Product ID {product_id} not found in the database.')
+    except Exception as e:
+        logger.error(f'An unexpected error occurred: {e}', exc_info=True)
+
+
 @transaction.atomic
 def handle_payment_failure(session):
     pass
